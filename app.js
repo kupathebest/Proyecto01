bf246183cf9dfd3f3c3f91e7b40acabaa93aabe0
chinosama <85371007+chinosama@users.noreply.github.com>
2021-06-08 18:10:23 -0300

    completa switch

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-const fs = require("fs");
-const tareas = require("./funcionesDeTareas")
+const process = require("process");
+const comando = process.argv[2];
+const titulo = process.argv[3];
+const estado = process.argv[4];
+const tareas = require("./funcionesDeTareas.js")
 
-const process = require("process")
 
-switch (process.argv[2]) {
+switch (comando) {
     case "agregar":
-        tareas.agregar();
-        break;
-
+        tareas.agregar(titulo,estado);
+        break;    
+    case "listar":
+        tareas.listar();
+        break; 
+        case undefined:
+            console.log("***Atencion - tienes que pasar una accion***");
+            break;
+        case 'filtrarEstado':
+          if(process.argv[3] == undefined){
+               console.log('***No hay resultados para tu búsqueda***');
+         }else{
+            console.log('***Estos son los estados que coinciden con tu busqueda***');
+            console.log(tareas.filtrarEstado(process.argv[3]));
+                };
+            break;
+        case "filtrarTitulo":
+            if(process.argv[3] == undefined){
+                console.log('***No hay resultados para tu búsqueda***');
+          }else{
+             console.log('***Estos son los titulos que coinciden con tu busqueda***');
+             console.log(tareas.filtrarTitulo(process.argv[3]));
+                 };
+            break;            
     default:
-        console.log("Chau");
+        console.log("No se que quieres hacer");
         break;
 }
