97b900663225860dd1e2e3b08dc461e0bb64d8a2
kupathebest <kupa.thebest@gmail.com>
2021-06-08 17:56:44 -0300

    agregando process y comenzando switch

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const fs = require("fs");
 const tareas = require("./tareas.json")
+
+const process = require("process")
+
+switch (process.argv[2]) {
+    case "hola":
+        console.log("Hola!!!!");
+        break;
+
+    default:
+        console.log("Chau");
+        break;
+}
