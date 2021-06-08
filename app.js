0a8165236df247e00422f359b21ebbae515ede08
kupathebest <kupa.thebest@gmail.com>
2021-06-08 18:06:13 -0300

    modulando y agregando primer caso

--- app.js
+++ app.js
@@ -1,41 +1,14 @@
 const fs = require("fs");
-const tareas = require("./tareas.json")
+const tareas = require("./funcionesDeTareas")
 
 const process = require("process")
 
 switch (process.argv[2]) {
-    case "hola":
-        console.log("Hola!!!!");
+    case "agregar":
+        tareas.agregar();
         break;
 
     default:
         console.log("Chau");
         break;
-}
-module.exports = {
-    tareas: JSON.parse(fs.readFileSync("./tareas.json","utf-8")),
-    agregar : function(titulo,estado){
-        let tarea = {
-            titulo,
-            estado,
-        }
-        this.tareas.push(tarea)
-        this.guardar(this.tareas)
-        this.mensaje('Estado de tarea agregado satisfactoriamente')
-
-    },
-guardar : function(tareas){
-    fs.writeFileSync("./tareas.json",JSON.stringify(tareas,null,2),"utf-8")
-},
-mensaje : texto => console.log(`******${texto}******`),
-
-filtrarEstado : (estado) => lista.filter(tarea => tarea.estado == estado),
-
-filtrarTitulo : (titulo) => lista.filter(tarea => tarea.titulo == titulo),
-
-listar : function (tareas = this.tareas){
-   for (let i = 0; i < tareas.length; i++) {
-       console.log(`Titulo: ${tareas[i].titulo}, Estado: ${tareas[i].estado}`);  
-   }
-}
 }
